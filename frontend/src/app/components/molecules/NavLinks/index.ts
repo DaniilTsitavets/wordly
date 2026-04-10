03a4958af2e89d9ecf0a8d0d654c386da3b3a361
Polina Trybialustava <polina_trybialustava@epam.com>
2026-04-10 12:47:54 +0300

    delete duplicates

--- frontend/src/app/components/molecules/NavLinks/index.ts
+++
@@ -1,4 +0,0 @@
-export { NavLinks } from './NavLinks'
-export { AuthenticatedNav } from './AuthenticatedNav'
-export { UnauthenticatedNav } from './UnauthenticatedNav'
-export type { NavLink } from './NavLinks.type'
