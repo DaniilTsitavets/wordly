b1947e5fabd001829d067b25812f41fbf756119e
Polina Trybialustava <polina_trybialustava@epam.com>
2026-04-27 16:59:56 +0300

    fix: removed unnecessary

--- frontend/src/components/organisms/Pages/SubTopicPage/SubTopicPage.tsx
+++ frontend/src/components/organisms/Pages/SubTopicPage/SubTopicPage.tsx
@@ -1,9 +1,3 @@
-import { FlashCardsPage } from '@/components/organisms/Pages/FlashCardsPage'
 export function SubTopicPage() {
-  return (
-    <>
-      <h1>SubTopicPage</h1>
-      <FlashCardsPage />
-    </>
-  )
+  return ()
 }
