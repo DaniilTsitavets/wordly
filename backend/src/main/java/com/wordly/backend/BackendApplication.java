e0de8371271c658bd4bafeccc524d43f430fcc43
Daniil Tsitavets <175641668+DaniilTsitavets@users.noreply.github.com>
2026-05-17 20:18:47 +0300

    fix: shift to application.yaml

--- backend/src/main/java/com/wordly/backend/BackendApplication.java
+++ backend/src/main/java/com/wordly/backend/BackendApplication.java
@@ -2,10 +2,9 @@ package com.wordly.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
     public static void main(String[] args) {
