7b7b73a729db89adad87c14cde43f8aa855638a7
Evgeniya Yankovich <evgeniya.yankovich@stud.esdc.lt>
2026-05-18 13:22:04 +0300

    Update AdminInitializer.java

--- backend/src/main/java/com/wordly/backend/config/AdminInitializer.java
+++ backend/src/main/java/com/wordly/backend/config/AdminInitializer.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
